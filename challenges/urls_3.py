c1126b1fcc4b8dbbca7c93b2884deb3b5f4b4731
Antonio de Odilon <83315460+antonioodilon@users.noreply.github.com>
2022-03-04 13:52:45 -0300

    Delete urls_3.py

--- challenges/urls_3.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from challenges import views
-
-urlpatterns = [
-    # The order matters. We first want to check for an int; otherwise, check for a string
-    path('<int:month>', views.monthly_challenge_by_number),  # If month is a number, we'll handle as an int
-    path('<str:month>', views.monthly_challenge_2),  # If the month is a string, we'll handle the string
-]
